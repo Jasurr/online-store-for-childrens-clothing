7ba42779fae238a775ef762da449f5d63f66c112
jasur <njasur.1995@gmail.com>
2023-07-09 20:12:01 +0500

    first commit

---
+++ src/main/java/com/onlinestoreforchildrensclothing/onlinestoreforchildrensclothing/OnlineStoreForChildrensClothingApplication.java
@@ -0,0 +1,15 @@
+package com.onlinestoreforchildrensclothing.onlinestoreforchildrensclothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreForChildrensClothingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreForChildrensClothingApplication.class, args);
+    }
+
+    //https://JasurNishonov@bitbucket.org/online-store-for-childrens-clothing/onlinestoreforchildrensclothing.git
+
+}
---
+++ src/test/java/com/onlinestoreforchildrensclothing/onlinestoreforchildrensclothing/OnlineStoreForChildrensClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestoreforchildrensclothing.onlinestoreforchildrensclothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreForChildrensClothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
